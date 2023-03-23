5e2822a57d4590b4d29214a196939b956e91988e
Denys Dashkevych <denysdashkevych@gmail.com>
2023-03-23 19:12:04 +0200

    update components

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,26 +6,15 @@ import { useMediaQuery } from 'react-responsive';
 import s from './Home.module.scss';
 
 import DailyCaloriesForm from '../DailyCaloriesForm';
-// import { UserInputData } from 'types/daily-rate.type';
 
 import bcgDesktop from '../../images/desktop/bcgD.png';
 import bcgDesktop2x from '../../images/desktop/bcgD@2x.png';
 
 const Home: FC = () => {
   const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
-
-  // const dispatch = useAppDispatch();
-
-  // const onGetDailyRate = (store: UserInputData) => {
-  //   const action = dailyRateInfo(store);
-  //   dispatch(action);
-  // };
-
   return (
     <section className={s.home}>
-      <DailyCaloriesForm 
-      // onSubmit={onGetDailyRate} 
-      />
+      <DailyCaloriesForm />
 
       {!isRetina && (
         <img className={s.imgDesktop} src={bcgDesktop} alt="background" />
