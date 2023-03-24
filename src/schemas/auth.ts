2297951f2c04e3109f903d571277099e16f0d05f
Denys Dashkevych <denysdashkevych@gmail.com>
2023-03-24 20:57:41 +0200

    update validation shemas

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -4,32 +4,32 @@ const emailRegexp = /^\w+[\w-.]*\w@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/;
 const registerSchema = yup.object().shape({
   username: yup
     .string()
-    .required("введіть ім'я")
-    .min(3, 'не менше ніж 3 символи')
-    .max(12, 'не більше ніж 12 символів'),
+    .required("Введіть ім'я")
+    .min(3, 'Не менше ніж 3 символи')
+    .max(12, 'Не більше ніж 12 символів'),
   email: yup
     .string()
-    .required("введіть електронну пошту")
-    .min(3, 'не менше ніж 3 символи')
-    .matches(emailRegexp, 'невалідна адреса'),
+    .required("Введіть електронну пошту")
+    .min(3, 'Не менше ніж 3 символи')
+    .matches(emailRegexp, 'Невалідна адреса'),
   password: yup
     .string()
-    .required('введіть пароль')
-    .min(8, 'не менше ніж 8 символів')
-    .max(100, 'не більше ніж 12 символів'),
+    .required('Введіть пароль')
+    .min(8, 'Не менше ніж 8 символів')
+    .max(100, 'Не більше ніж 12 символів'),
 });
 
 const loginSchema = yup.object().shape({
   email: yup
     .string()
-    .required("введіть електронну пошту")
-    .min(3, 'не менше ніж 3 символи')
-    .matches(emailRegexp, 'невалідна адреса'),
+    .required("Введіть електронну пошту")
+    .min(3, 'Не менше ніж 3 символи')
+    .matches(emailRegexp, 'Невалідна адреса'),
   password: yup
     .string()
-    .required('введіть пароль')
-    .min(8, 'не менше ніж 8 символів')
-    .max(100, 'не більше ніж 12 символів'),
+    .required('Введіть пароль')
+    .min(8, 'Не менше ніж 8 символів')
+    .max(100, 'Не більше ніж 12 символів'),
 });
 
 const schema = {
--- src/schemas/daily-form.ts
+++ src/schemas/daily-form.ts
@@ -5,26 +5,26 @@ const schema = yup.object().shape({
     .number()
     .typeError('Введіть число')
     .required()
-    .min(100, 'не менше ніж 100')
-    .max(250, 'не більше ніж 250'),
+    .min(100, 'Не менше ніж 100')
+    .max(250, 'Не більше ніж 250'),
   age: yup
     .number()
     .typeError('Введіть число')
     .required()
-    .min(18, 'не менше ніж 18')
-    .max(100, 'не більше ніж 100'),
+    .min(18, 'Не менше ніж 18')
+    .max(100, 'Не більше ніж 100'),
   weight: yup
     .number()
     .typeError('Введіть число')
     .required()
-    .min(40, 'не менше ніж 40')
-    .max(250, 'не більше ніж 250'),
+    .min(40, 'Не менше ніж 40')
+    .max(250, 'Не більше ніж 250'),
   desiredWeight: yup
     .number()
     .typeError('Введіть число')
     .required()
-    .min(40, 'не менше ніж 40')
-    .max(250, 'не більше ніж 250'),
+    .min(40, 'Не менше ніж 40')
+    .max(250, 'Не більше ніж 250'),
   bloodType: yup.string().required('Оберіть число').required('Оберіть число'),
 });
 
