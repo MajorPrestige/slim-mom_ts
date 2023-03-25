c65f124a9113c52acc93cc869c5c2979b6312dcc
Denys Dashkevych <denysdashkevych@gmail.com>
2023-03-25 16:47:28 +0200

    Update LoaderMini.tsx

--- src/components/LoaderMini/LoaderMini.tsx
+++ src/components/LoaderMini/LoaderMini.tsx
@@ -3,7 +3,7 @@ import { Oval } from 'react-loader-spinner';
 
 const Loader: FC = () => {
   return (
-    <div className="flex, justify-center items-center h-[200px] bg-main-color">
+    <div className="flex justify-center mt-2.5 items-center bg-main-color tablet-before:mt-10">
       <Oval height="50" width="50" color="#fc842d" secondaryColor="transparent" />
     </div>
   );
